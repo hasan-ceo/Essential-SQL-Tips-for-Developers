5640e9746046b3f3cb46771100837f913cc4ab68
Hasan Habib <surzo4368@yahoo.com>
2020-10-10 02:16:30 +0600

    Update EmptyAllTable.sql

--- EmptyAllTable.sql
+++ EmptyAllTable.sql
@@ -5,7 +5,8 @@ step 1: Read all table name from INFORMATION_SCHEMA.TABLES
 step 2: Format column value as required in this case we add "delete from " string.
 step 3: Use the cursor to retrieve data, one row at a time, from our result set
 Step 4: Execute our formatted column values as a dynamic query for each iteration inside the cursor
-other scope: This same process can follow in MySQL or any other relational database. You only need to convert it into the language-specific syntax
+other scope: This same process can follow in MySQL or any other relational database. 
+You only need to convert it into the language-specific syntax
 */		
 declare @str varchar(250)
 declare tmp cursor for
