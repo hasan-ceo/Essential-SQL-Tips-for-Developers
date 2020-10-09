dfbd170ebecf74dd6352a43e5d5bc25ee5ab71e1
Hasan Habib <surzo4368@yahoo.com>
2020-10-10 02:09:57 +0600

    Update EmptyAllTable.sql

--- EmptyAllTable.sql
+++ EmptyAllTable.sql
@@ -1,13 +1,22 @@
-		declare @str varchar(250)
-		declare tmp cursor for
-		SELECT 'delete from '+table_schema+'.'+TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+/* 
+Empty All Table
+database: Microsoft SQL Server 
+step 1: Read all table name from INFORMATION_SCHEMA.TABLES
+step 2: Format column value as required in this case we add "delete from " string.
+step 3: Use cursor to retrieve data, one row at a time, from a our result set
+Step 4: Execute our formated column values as dynamic query for each iteration inside cursor
+other scope: This same process can follow in MySQL or any other relational database. You only need to convert it into the language-specific syntax
+*/		
+declare @str varchar(250)
+declare tmp cursor for
+SELECT 'delete from '+table_schema+'.'+TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
 
-		open tmp
-		fetch next from tmp into @str
-		while @@fetch_status = 0 
-		begin
-			EXECUTE (@str)
-			fetch next from tmp into @str
-		end
-		close tmp
-		deallocate tmp
+open tmp
+fetch next from tmp into @str
+while @@fetch_status = 0 
+begin
+	EXECUTE (@str)
+	fetch next from tmp into @str
+end
+close tmp
+deallocate tmp
